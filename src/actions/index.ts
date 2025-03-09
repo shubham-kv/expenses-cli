ab9572d9ad15fa3c699523171bc8f66d9d6e9c22
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-03-09 14:24:01 +0530

    refactor(program): use commander js extra typings pkg

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,4 @@
-export { addExpenseAction as addExpense } from "./add-expense";
+export { addExpenseAction } from "./add-expense";
 export { deleteExpenseAction as deleteExpense } from "./delete-expense";
 export { listExpenses } from "./list-expenses";
 export { updateExpenseAction as updateExpense } from "./update-expense";
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,32 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
+import { Command } from "@commander-js/extra-typings";
 import { name, description, version } from "../package.json";
 
 import {
-  addExpense,
+  addExpenseAction,
   updateExpense,
   deleteExpense,
   listExpenses,
   viewExpenseSummary,
 } from "./actions";
 
-const program = new Command();
-
-program.name(name).description(description).version(version);
+const program = new Command()
+  .name(name)
+  .description(description)
+  .version(version);
 
 program
-  .command("add <name>")
-  .description("Add a new expense.")
+  .command("add")
+  .argument("<name>", "Expense name")
+  .description("Add a new expense")
   .option("-d, --description <description>", "Text describing the expense")
-  .option("-a, --amount <amount>", "Expense amount", parseFloat)
-  .action(addExpense);
+  .requiredOption(
+    "-a, --amount <amount>",
+    "Expense amount, a positive number"
+  )
+  // @ts-ignore
+  .action(addExpenseAction);
 
 program
   .command("edit <id>")
@@ -28,6 +34,7 @@ program
   .option("-n, --name <name>", "Name of the expense")
   .option("-d, --description <description>", "Text describing the expense")
   .option("-a, --amount <amount>", "Expense amount", parseFloat)
+  // @ts-ignore
   .action(updateExpense);
 
 program
@@ -48,6 +55,7 @@ program
     "Number representing the month of the current year to show the summary for, from 1 - 12, 1 for January, 12 for December",
     parseInt
   )
+  // @ts-ignore
   .action(viewExpenseSummary);
 
 program.parse(process.argv);
